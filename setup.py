9887b3f4d9ba8220fd4db3661c66f04bdf24144b
mfsuve <suvemustafa@gmail.com>
2023-10-16 19:09:55 +0300

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name="llm_transcript_analysis",
+    version="1.0",
+    packages=["llm_transcript_analysis"],
+    package_dir={"llm_transcript_analysis": "src/llm_transcript_analysis"},
+)
