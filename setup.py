b7402f8b9b3c33d07abc4e0ab623b44b5e6b7d34
mfsuve <suvemustafa@gmail.com>
2023-10-17 14:42:47 +0300

    Added required dependencies
    * transformers
    * tokenizers
    * safetensors

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     version="1.0",
     packages=["llm_transcript_analysis"],
     package_dir={"llm_transcript_analysis": "src/llm_transcript_analysis"},
+    install_requires=[
+        "transformers==4.34.0",
+        "tokenizers==0.14.1",
+        "safetensors==0.4.0",
+    ],
 )
